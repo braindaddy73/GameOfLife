54ce30b4ba4b36ff54b286b1c6e8a5f08a988923
braindaddy73 <heiko@kitscha.de>
2024-04-09 09:05:20 +0200

    initial commit

---
+++ src/Biotop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Heiko Kitscha on 07.04.24.
+//
+
+#include "Biotop.h"
---
+++ src/Biotop.h
@@ -0,0 +1,14 @@
+//
+// Created by Heiko Kitscha on 07.04.24.
+//
+
+#ifndef GAMEOFLIFE_BIOTOP_H
+#define GAMEOFLIFE_BIOTOP_H
+
+
+class Biotop {
+
+};
+
+
+#endif //GAMEOFLIFE_BIOTOP_H
