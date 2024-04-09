b32e8dada4f95310c2ed2e5f624134af7ee1ddf4
braindaddy73 <heiko@kitscha.de>
2024-04-09 14:52:50 +0200

    Update Biotope.h

--- src/Biotope.h
+++ src/Biotope.h
@@ -8,21 +8,34 @@
 #include<wx/dcmemory.h>
 #include<vector>
 
+namespace Biotope {
+
 //! Enumeration with the implemented rules
 enum Rules {
     STANDARD,    //! Standard rule of Conways Game of Life
     COUNTMOD2    //! Alternate rule "Count modulo 2"
 };
 
+//! Enumration with Styles
+enum Style {
+    PLANE,
+    TOROID
+}
+
+} // End of namespace biotope
+
+
 //! A class to hold the biotope, to calculate generations and draw on a wxMemoryDC
 class Biotope {
 
 private:
     //! Vector to hold the biotope
-    std::vector<std::vector<int> biotope;
-    std::vector<std::vector<int> buffer;
-    int width;
-    int height;
+    std::vector<std::vector<int>> biotope(100, std::vector<int>(100, 0));
+    std::vector<std::vector<int>> buffer (100, std::vector<int>(100, 0));
+    int width = 100;
+    int height = 100;
+    //! PADDING: 1 = surrounded by sulfur acid, means plane biotope; 0 = toroid (last is neighbour of first)
+    constexpr PADDING = 1;
     void calcConway();
     void calcCM2();
     int getNeighbourCount(int x, int y);
