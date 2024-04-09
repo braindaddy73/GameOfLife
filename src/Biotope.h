92ee695dda215569c1ef9ab5f6492ebd9fe31a49
braindaddy73 <heiko@kitscha.de>
2024-04-09 12:20:40 +0200

    Update Biotope.h

--- src/Biotope.h
+++ src/Biotope.h
@@ -5,17 +5,45 @@
 #ifndef GAMEOFLIFE_BIOTOPE_H
 #define GAMEOFLIFE_BIOTOPE_H
 
+#include<wx/dcmemory.h>
+#include<vector>
 
+//! Enumeration with the implemented rules
+enum {
+    STANDARD,    //! Standard rule of Conways Game of Life
+    COUNTMOD2    //! Alternate rule "Count modulo 2"
+} Rules;
+
+//! A class to hold the biotope, to calculate generations and draw on a wxMemoryDC
 class Biotope {
+
 private:
-    std::vector<std::vector<unsigned char>> biotope;
+    //! Vector to hold the biotope
+    std::vector<std::vector<int> biotope;
+    std::vector<std::vector<int> buffer;
+    int width;
+    int height;
+    void calcConway();
+    void calcCM2();
 
 public:
-    Biotope();
+    Biotope(int width, int height);
     ~Biotope();
-
-    calcNextGen();
-    unsigned char getCell(int x, int y);
+    //! Calculates the nex generation with a given rule and returns the millis
+    int calcNextGen(Rules rule);
+    //! Function to get the value of a specific cell
+    int getCell(int x, int y);
+    //! Function to draw the actual biotope onto w wxMemoryDC    
+    void draw(wxMemoryDC& dc);
+    //! Erases all life in the actual biotope
+    void clear();
+    //! Fills the biotope with a pinch of cells
+    /*! If cellCount > width x height, it is set to width x height */
+    void randomize(int cellCount);
+    //! Resizes the actual biotope
+    void resize(int x, int y);    
+    int getWidth();
+    int getHeight();
 };
 
 
